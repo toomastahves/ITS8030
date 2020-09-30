74bed10c2662d4ce9aa38199511a7a5c9e6c3454
Toomas Tahves <toomastahves@hotmail.com>
2020-09-30 15:54:39 +0300

    feat: add gaussian

--- homework1/ex2.py
+++ homework1/ex2.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import os
 import math
+import matplotlib.pyplot as plt
+
 os.chdir('C:\\Projects\\ITS8030\\homework1')
 
 """
@@ -26,9 +28,21 @@ def gaussian_blur_image(image : np.ndarray, sigma : float, in_place : bool = Fal
     radius = int(math.ceil(3 * sigma))
     kernel_size = 2 * radius + 1
     gaussian_filter = np.zeros((kernel_size, kernel_size))
-    print(gaussian_filter)
-    return image
+
+    m = int(gaussian_filter.shape[0] / 2)
+    n = int(gaussian_filter.shape[1] / 2)
+
+    for x in range(-m, m+1):
+        for y in range(-n, n+1):
+            res1 = 1 / (2*np.pi*sigma**2)
+            res2 = np.exp(-(x**2+y**2)/(2*sigma**2))
+            gaussian_filter[x+m, y+n] = res1 * res2
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(gaussian_filter)
+    plt.show()
+    return gaussian_filter
 
 img_input = cv2.imread('songfestival.jpg', cv2.IMREAD_GRAYSCALE)
-img_output = gaussian_blur_image(img_input, 2)
+img_output = gaussian_blur_image(img_input, 4)
 cv2.imwrite('task2.png', img_output)
