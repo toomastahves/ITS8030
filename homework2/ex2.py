ae02c6ee0454e5a2ced56b270c1fa94b190b3ab0
Toomas Tahves <toomastahves@hotmail.com>
2020-11-09 18:47:17 +0200

    feat: add cropping

--- homework2/ex2.py
+++ homework2/ex2.py
@@ -34,10 +34,22 @@ def parseCoordinates(data):
     body = boundingBoxes[1]
     return breathingHole, body
 
+# Draw labeling boxes on images
 for i in range(0, len(label_files)):
     with open(label_path + label_files[i], 'r') as json_file:
+        # Load coordinates
         breathingHole, body = parseCoordinates(json.load(json_file)[0])
+        # Load images
         image = Image.open(image_path + image_files[i])
+        # Crop body
+        image_cropped = image.crop((body["left"], body["top"], 
+            body["left"]+body["width"], body["top"]+body["height"]))
+        image_cropped.save('homework2/templates/body/' + image_files[i])
+        # Crop breathing hole
+        image_cropped = image.crop((breathingHole["left"], breathingHole["top"], 
+            breathingHole["left"]+breathingHole["width"], breathingHole["top"]+breathingHole["height"]))
+        image_cropped.save('homework2/templates/breathingHole/' + image_files[i])
+        # Draw boxes and save
         drawBrethingHoleRectangle(image)
         drawBodyRectangle(image)
         image.save(labeled_images_path + image_files[i])
